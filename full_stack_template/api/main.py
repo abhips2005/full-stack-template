8012e8859495b37ffbcfca694740ecf9ea1ffe66
Abhijith S <abhijiths2028@cs.sjcetpalai.ac.in>
2025-02-19 19:27:48 +0530

    Delete full_stack_template directory

--- full_stack_template/api/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from api.routes import status  # Add your core routes
-
-api_router = APIRouter()
-api_router.include_router(status.router)
-# Add your core routes here
--- full_stack_template/utils/typer.py
+++
@@ -1,4 +0,0 @@
-from typing import TypedDict, Dict, Any
-
-def typeof(initial_data: Dict[str, Any]):
-    return TypedDict('DynamicData', {k: type(v) for k, v in initial_data.items()})
